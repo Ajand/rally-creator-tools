85c780cc2b5e6a862181e578276f41f1c5cc1a2f
Ajand Mardalizad <aseedgeek@gmail.com>
2022-03-11 22:50:38 +0330

    images and text images poll options

--- panel/src/components/PollWidget.js
+++ panel/src/components/PollWidget.js
@@ -1,5 +1,6 @@
 import { createUseStyles } from "react-jss";
 import GoogleFontLoader from "react-google-font-loader";
+import { Row, Col } from "react-grid-system";
 
 const useStyles = createUseStyles({
   root: {
@@ -21,6 +22,43 @@ const useStyles = createUseStyles({
     boxShadow: "3px 3px black",
     cursor: "pointer",
   },
+  img: {
+    width: "100%",
+    borderRadius: 10,
+    border: "3px solid black",
+    boxShadow: "3px 3px black",
+    marginBottom: '0.5em'
+    //padding: '1em',
+  },
+
+  imageOptionsContainer: {
+    //padding: "8px",
+    marginTop: "1em",
+    marginBottom: "1em",
+    borderRadius: "8px",
+    cursor: "pointer",
+  },
+  optionImageContainer: {
+    boxSizing: "border-box",
+    boxShadow: "3px 3px black",
+    border: "3px solid black",
+    borderRadius: 10,
+    marginTop: "1em",
+    marginBottom: "1em",
+    marginBottom: "1em",
+    borderRadius: "8px",
+    overflow: "hidden",
+  },
+  imgWithText: {
+    width: "100%",
+    //  border: "3px solid black",
+    boxSizing: "border-box",
+  },
+  optionImageText: {
+    borderTop: "3px solid black",
+    padding: "1em",
+    marginTop: "-4px",
+  },
 });
 
 const PollWidget = ({ poll }) => {
@@ -41,10 +79,46 @@ const PollWidget = ({ poll }) => {
     );
   };
 
+  const imageOptions = () => {
+    return (
+      <div className={classes.imageOptionsContainer}>
+        {poll.basics.options.map((op, i) => (
+          <img
+            className={classes.img}
+            src={`https://ipfs.io/ipfs/${op.hash}`}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const textImageOptions = () => {
+    return (
+      <div className={classes.optionsContainer}>
+        {poll.basics.options.map((op, i) => (
+          <div
+            style={{ background: i == 0 ? "#17b3e2" : "white" }}
+            className={classes.optionImageContainer}
+          >
+            <img
+              className={classes.imgWithText}
+              src={`https://ipfs.io/ipfs/${op.hash}`}
+            />
+            <div className={classes.optionImageText}>{op.body}</div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   const renderProper = () => {
     switch (poll.basics.variant) {
       case "t":
         return textOptions(); //<div>Hello</div>;
+      case "i":
+        return imageOptions(); //<div>Hello</div>;
+      case "ti":
+        return textImageOptions(); //<div>Hello</div>;
     }
   };
 
